2e2b5eee45e01cea1ffd12099a28ad21111df787
Vivekshrivastava1912 <shrivastavavivek19d02005@gmail.com>
2026-04-09 17:54:35 +0530

    Fixed: client and server folders included properly

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <>
+    <div className='bg-red-200 text-8xl'>me app component hu 
+
+    </div>
+    </>
+  )
+}
+
+export default App
