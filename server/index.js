a7b3719c96324468e3d31e9467eff02f4d89f0df
Vivekshrivastava1912 <shrivastavavivek19d02005@gmail.com>
2026-04-09 17:58:16 +0530

    server-github-cheack

--- server/index.js
+++ server/index.js
@@ -31,8 +31,8 @@ app.get('/test', (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`-----------------------------------------`);
+  
     console.log(`⚡ server is running successfully`);
     console.log(`🔗 URL: http://localhost:${PORT}`);
-    console.log(`-----------------------------------------`);
+   
 });
